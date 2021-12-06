8b1ca2da66f890292f8f9b8df1f3c13b803f28a6
Rafanzhani Elfarizy <rafanzhani.elfarizy@btpn.com>
2021-12-06 13:39:11 +0700

    [Rafan, Gris] initialize project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+
+const App = function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h3>list of contact</h3>
+      <ul>
+        <li>
+          John
+          <br />
+          {' '}
+          081220488765
+        </li>
+        <li>
+          Bob
+          <br />
+          {' '}
+          08122048889
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
