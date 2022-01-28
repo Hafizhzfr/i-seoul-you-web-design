a56bc5886fcb1e624de843d3eb625eb15eebe98b
HAFIZH.ZUFAR <hafizh.zufar@btpn.com>
2022-01-28 20:39:13 +0700

    first init

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import ButtonTest from './ButtonTest/ButtonTest';
-import MathForm from './MathForm/MathForm';
-import CurriculumVitae from './CurriculumVitae/CurriculumVitae';
-import ShoppingList from './ShoppingList/ShoppingList';
 import './App.css';
 
 const App = () => (
   <div>
     <nav className="navbar">
       <div className="nav-text">
-        <Link className="link-text" to="/math">CALCULATOR</Link>
         <Link className="link-text" to="/increments">INCREMENTS</Link>
-        <Link className="link-text" to="/curriculum-vitae">CV GENERATOR</Link>
-        <Link className="link-text" to="/shopping-list">SHOPPING LIST</Link>
       </div>
     </nav>
 
     <Routes>
       <Route path="/increments" element={<ButtonTest />} />
-      <Route path="/math" element={<MathForm />} />
-      <Route path="/curriculum-vitae" element={<CurriculumVitae />} />
-      <Route path="/shopping-list" element={<ShoppingList />} />
     </Routes>
   </div>
 );
