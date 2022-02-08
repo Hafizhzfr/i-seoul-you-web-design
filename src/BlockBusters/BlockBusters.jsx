d5a7871a194a9dfd7bd130368352d7d7897bba0b
HAFIZH.ZUFAR <hafizh.zufar@btpn.com>
2022-02-08 11:23:19 +0700

    design rev

--- src/BlockBusters/BlockBusters.jsx
+++ src/BlockBusters/BlockBusters.jsx
@@ -20,28 +20,28 @@ const BlockBusters = () => {
     pics: silentSeaPics,
     starring: 'Bae Doona, Gong Yoo',
     title: 'The Silent Sea',
-    netflixLink: 'https://open.spotify.com/album/3ma5amx5s3l1NKoWNHaMYe'
+    netflixLink: 'https://www.netflix.com/id-en/title/81098012'
   };
 
   const aouad = {
     pics: aouadPics,
     starring: 'Park Solomon, Lee Yoo-Mi, Cho Yi-hyun',
     title: 'All of Us Are Dead',
-    netflixLink: 'https://open.spotify.com/album/3ma5amx5s3l1NKoWNHaMYe'
+    netflixLink: 'https://www.netflix.com/id/title/81237994'
   };
 
   const obs = {
     pics: obsPics,
     starring: 'Kim Da-mi, Choi Woo-shik',
     title: 'Our Beloved Summer',
-    netflixLink: 'https://open.spotify.com/album/3ma5amx5s3l1NKoWNHaMYe'
+    netflixLink: 'https://www.netflix.com/id-en/title/81486372'
   };
 
   const myName = {
     pics: myNamePics,
     starring: 'Han So Hee, Ahn Bo-hyun, Park Hee Soon',
     title: 'My Name',
-    netflixLink: 'https://open.spotify.com/album/3ma5amx5s3l1NKoWNHaMYe'
+    netflixLink: 'https://www.netflix.com/id-en/title/81011211'
   };
   //= =============================================================================================
 
