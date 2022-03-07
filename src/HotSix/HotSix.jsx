9af570ca814de65d0f024f46a73d19b99499d7ec
HAFIZH_ZUFAR <hafizhzufar1@gmail.com>
2022-03-07 23:22:43 +0700

    add dbjson

--- src/HotSix/HotSix.jsx
+++ src/HotSix/HotSix.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import gidlePics from '../pics/gidle-hwaa.png';
-import secondYukikaPics from '../pics/yukika-timeabout.png';
-import yerinPics from '../pics/yerin.png';
-import eyediPics from '../pics/eyedi-cover.png';
-import yukikaPics from '../pics/yukika-soullady.png';
 import bibiPics from '../pics/bibi-lifeisabi.png';
+import eyediPics from '../pics/eyedi-cover.png';
+import gidlePics from '../pics/gidle-hwaa.png';
+import yerinPics from '../pics/yerin.png';
+import yukikaPics from '../pics/yukika-soullady.png';
+import secondYukikaPics from '../pics/yukika-timeabout.png';
 import './HotSix.css';
 
 const HotSix = () => {
@@ -13,7 +13,12 @@ const HotSix = () => {
     artist: '(G)I-DLE',
     title: 'I-Burn',
     songs: [
-      'HANN (Lost in the winter)', 'HWAA', 'MOON', 'Where Is Love', 'LOST', 'DAHLIA'
+      'HANN (Lost in the winter)',
+      'HWAA',
+      'MOON',
+      'Where Is Love',
+      'LOST',
+      'DAHLIA'
     ],
     spotifyLink: 'https://open.spotify.com/album/3ma5amx5s3l1NKoWNHaMYe'
   };
@@ -22,7 +27,12 @@ const HotSix = () => {
     artist: 'Yukika',
     title: 'timeabout,',
     songs: [
-      'Leap forward', 'Insomnia', 'Love Month', 'TIME TRAVEL', 'Secret', 'PUNG!'
+      'Leap forward',
+      'Insomnia',
+      'Love Month',
+      'TIME TRAVEL',
+      'Secret',
+      'PUNG!'
     ],
     spotifyLink: 'https://open.spotify.com/album/1cBAfX0otvkoIOI6HtOrAc'
   };
@@ -30,18 +40,14 @@ const HotSix = () => {
     pics: yerinPics,
     artist: 'Baek Yerin',
     title: 'Love, Yerin',
-    songs: [
-      'Whenever', 'Antifreeze', 'Go Back', 'Why Me?', 'Limit', 'A Walk'
-    ],
+    songs: ['Whenever', 'Antifreeze', 'Go Back', 'Why Me?', 'Limit', 'A Walk'],
     spotifyLink: 'https://open.spotify.com/album/2K41KAlW6n9bVlRCQPVcSZ'
   };
   const eyedi = {
     pics: eyediPics,
     artist: 'Eyedi',
     title: '&NEW',
-    songs: [
-      '&NEW', 'Caffeine', 'J.us.T', 'Best Mistake - K', 'Luv Highway'
-    ],
+    songs: ['&NEW', 'Caffeine', 'J.us.T', 'Best Mistake - K', 'Luv Highway'],
     spotifyLink: 'https://open.spotify.com/artist/48BSHaU3lXnM3pYHyeqLXN'
   };
   const yukika = {
@@ -49,7 +55,16 @@ const HotSix = () => {
     artist: 'Yukika',
     title: 'Soul Lady',
     songs: [
-      'SOUL LADY', 'Neon', 'I FEEL LOVE', 'From HND to GMP', 'Yesterday', 'A Day for Love', 'pit-a-pet', 'NEON 1989', 'Cherries Jubiles', 'SHADE'
+      'SOUL LADY',
+      'Neon',
+      'I FEEL LOVE',
+      'From HND to GMP',
+      'Yesterday',
+      'A Day for Love',
+      'pit-a-pet',
+      'NEON 1989',
+      'Cherries Jubiles',
+      'SHADE'
     ],
     spotifyLink: 'https://open.spotify.com/album/16yrp3d9pCJgQK2RMBTtd1'
   };
@@ -58,7 +73,11 @@ const HotSix = () => {
     artist: 'BiBi',
     title: 'Life is a Bi...',
     songs: [
-      'Umm... Life', 'BAD SAD AND MAD', 'PIRI the dog', 'Birthday Cake', 'Life is a Bi...'
+      'Umm... Life',
+      'BAD SAD AND MAD',
+      'PIRI the dog',
+      'Birthday Cake',
+      'Life is a Bi...'
     ],
     spotifyLink: 'https://open.spotify.com/album/0VDb4oas0usbuPljrSZH5s'
   };
@@ -74,7 +93,13 @@ const HotSix = () => {
   const hotSix = '//////  HOT SIX';
   const hotSixList = [gidle, bibi, yukika, eyedi, yerin, secondYukika];
 
-  const handleDetailClick = (picInput, artistInput, songsInput, titleInput, spotifyInput) => {
+  const handleDetailClick = (
+    picInput,
+    artistInput,
+    songsInput,
+    titleInput,
+    spotifyInput
+  ) => {
     setArtist(artistInput);
     setPic(picInput);
     setSongs(songsInput);
@@ -96,7 +121,9 @@ const HotSix = () => {
             <h1>{artist}</h1>
             <h2>{title}</h2>
             <a href={spotify} target="_blank" rel="noreferrer">
-              <button className="spotify-button" type="button">Listen on Spotify</button>
+              <button className="spotify-button" type="button">
+                Listen on Spotify
+              </button>
             </a>
             {songs.map((song) => (
               <ul className="songs-list">
@@ -104,9 +131,14 @@ const HotSix = () => {
               </ul>
             ))}
           </div>
-          <button className="album-detail-button" type="button" onClick={handleReturnClick}>X</button>
+          <button
+            className="album-detail-button"
+            type="button"
+            onClick={handleReturnClick}
+          >
+            X
+          </button>
         </div>
-
       );
     }
     return '';
@@ -118,7 +150,11 @@ const HotSix = () => {
       <div className="album-row">
         {hotSixList.map((album) => {
           const {
-            pics: picsMenu, artist: artistMenu, title: titleMenu, songs: songsMenu, spotifyLink
+            pics: picsMenu,
+            artist: artistMenu,
+            title: titleMenu,
+            songs: songsMenu,
+            spotifyLink
           } = album;
           return (
             <div className="album-column">
@@ -126,7 +162,17 @@ const HotSix = () => {
               <div className="album-description">
                 <p className="album-detail">{artistMenu}</p>
                 <p className="album-detail">{titleMenu}</p>
-                <button className="album-detail" type="button" onClick={() => handleDetailClick(picsMenu, artistMenu, songsMenu, titleMenu, spotifyLink)}>
+                <button
+                  className="album-detail"
+                  type="button"
+                  onClick={() => handleDetailClick(
+                    picsMenu,
+                    artistMenu,
+                    songsMenu,
+                    titleMenu,
+                    spotifyLink
+                  )}
+                >
                   Check Details!
                 </button>
               </div>
